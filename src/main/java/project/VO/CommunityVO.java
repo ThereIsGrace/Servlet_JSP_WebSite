0823f641f48ab0e975495b22c1e18a215aec60ec
ThereIsGrace <universe2879@gmail.com>
2023-06-20 21:26:33 +0900

    [communityVO] 생성자 추가

--- src/main/java/project/VO/CommunityVO.java
+++ src/main/java/project/VO/CommunityVO.java
@@ -1,12 +1,24 @@
 package project.VO;
 
-import java.sql.Date;
+import java.util.Date;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 
 public class CommunityVO {
+	public CommunityVO() {
+	}
+	
+	public CommunityVO(int comarticleno, String comtitle, String comwriter_id, String comcontent, Date comwriter_date) {
+		this.comarticleno = comarticleno;
+		this.comtitle = comtitle;
+		this.comwriter_id = comwriter_id;
+		this.comcontent = comcontent;
+		this.comwriter_date = comwriter_date;
+	}
+
 	private int comarticleno;
 	private String comtitle;
 	private String comwriter_id;
