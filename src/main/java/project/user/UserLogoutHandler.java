32271780ec8a1c445302d41f825986a2b4f24e30
Parktaesunkr <spcsm5452@naver.com>
2023-06-22 19:06:44 +0900

    로그인 기능 세션 추가

---
+++ src/main/java/project/user/UserLogoutHandler.java
@@ -0,0 +1,36 @@
+package project.user;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import project.DAO.LoginDAO;
+
+
+@WebServlet("/lo.do")
+public class UserLogoutHandler extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandler(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	doHandler(request, response);
+	}
+	protected void doHandler(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		
+		request.getSession().invalidate();
+		RequestDispatcher dispatch = request.getRequestDispatcher("/index.html");
+		dispatch.forward(request, response);
+		
+	}
+
+}
