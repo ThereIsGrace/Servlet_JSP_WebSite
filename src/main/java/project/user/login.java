8351d1a051e9d05f35ea5dcd96a6e99246c2009d
Parktaesunkr <spcsm5452@naver.com>
2023-06-23 14:03:58 +0900

    css 수정, 발생 문제 수정, 주석 첨가

--- src/main/java/project/user/login.java
+++ src/main/java/project/user/login.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
 
@@ -30,12 +31,8 @@ public class login extends HttpServlet {
 	String user_id=request.getParameter("user_id");
 	String user_password=request.getParameter("user_password");
 	
-	Session session = (Session)request.getSession();
-	
-
-	
-	
 	
 	}
+
 
 }
